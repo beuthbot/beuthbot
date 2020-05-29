d209f259e173bc15460875574c68619daf64aff6
Lukas Danckwerth <lukas.danckwerth@winvalue.de>
2020-05-29 21:46:34 +0200

    add update.sh for convenien deploying

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# short and simple bash script for simplifying deployment process on
+# on productive machine.  make shure there is a `.env` file specifying
+# the telegram token before running this script.
+
+# print commands
+set -x
+
+# reset git project
+git reset HEAD --hard
+
+# fetch latest git update
+git pull
+
+# update submodules
+git submodule update
+
+# rebuild and start docker container
+docker-compose up --build -d
