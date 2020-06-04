831640d8ee3056b8c6ced1434e325038d6c95ece
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-06-04 19:14:41 +0200

    update update.sh to check for existence of used commands

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
+# print commands
+set -x
+
 # short and simple bash script for simplifying deployment process on
 # on productive machine.  make shure there is a `.env` file specifying
 # the needed endpoints and tokens.
 
-# print commands
-set -x
+
+# check preconditions
+if ! foobar_loc="$(type -p "git")" || [[ -z $foobar_loc ]]; then
+   echo "Can't find git command. Is it installed?" && exit 0
+fi
+
+if ! foobar_loc="$(type -p "docker-compose")" || [[ -z $foobar_loc ]]; then
+   echo "Can't find docker-compose command. Is it installed?" && exit 0
+fi
+
 
 # reset git project
 git reset HEAD --hard
 
-# fetch latest git update
-git pull
+# reset submodules (including nested submodules)
+git submodule foreach --recursive 'git reset --hard'
 
-# update submodules
-git submodule update
+# pull all changes in the repo including changes in the submodules
+git pull --recurse-submodules
 
 # rebuild and start docker container
 docker-compose up --build -d
