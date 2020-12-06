2c9b5857566410f35580cc0c2780fde7b1cf1101
s79912 <s79912@beuth-hochschule.de>
2020-12-06 14:57:53 +0100

    cats microservice

---
+++ cat_microservice/src/index.ts
@@ -0,0 +1,44 @@
+/**
+ *
+ * Cat Micrososervice is a tiny microservice for BHT-Bot
+ *
+ * It is meant for showcasing how a BHT-Bot Service with the
+ * newly created @bhtbot/bhtbotservice Framework might look like.
+ *
+ * Also it's the first service implemented in Typescript, so this
+ * might be a good starting point / boilerplate if you plan to create
+ * another BHT-Bot Service
+ *
+ * Contributers
+ * - Dennis Walz 2020
+ */
+
+/* Init environment from .env for non-docker */
+import {config as dotenvConfig} from 'dotenv';
+dotenvConfig();
+
+/* Init Service */
+import {Service, AppConfig} from '@bhtbot/bhtbotservice';
+const config = new AppConfig();
+config.port = process.env.CATS_PORT ? Number(process.env.CATS_PORT) : 3000;
+const app = new Service('catService', config);
+
+/* Init axios with auth header for cat-api */
+const axios = require('axios');
+axios.defaults.headers.common['x-api-key'] = process.env.CATS_APIKEY
+const catAPIendpoint = 'https://api.thecatapi.com/v1/images/search';
+
+/* Query api image */
+const queryCat = async ()=>{
+    let response = await axios.get(catAPIendpoint, { params: { limit:1, size:"full" } } )
+    return { id: response.data[0].id, url: response.data[0].url}
+}
+
+/* Listen on endpoint /cat */
+app.endpoint('cat', async (req, answ)=>{
+    answ.message = (await queryCat()).url;
+    return answ;
+})
+
+/* Start server */
+app.start();
