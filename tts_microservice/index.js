b6ba325824741fe6a5615053610740489c6cdb3e
apbln <38110721+apbln@users.noreply.github.com>
2021-02-03 19:56:17 +0100

    Added TTS-Service

---
+++ tts_microservice/index.js
@@ -0,0 +1,31 @@
+const ttsService = require('./services/ttsService');
+const ttsRouter = require('./services/tts');
+
+const {Service, AppConfig, FileAnswer} = require('@bhtbot/bhtbotservice');
+
+/* Init Service */
+const config = new AppConfig();
+config.port = process.env.PORT ? Number(process.env.PORT) : 3003;
+const app = new Service('ttsService', config);
+
+/* Listen on endpoint /tts_microservice */
+app.endpoint('tts_microservice', async (req, answ) => {
+
+    let text = req.content// content from STT
+    let lang = 'de'
+    let name = "tts_test"
+    let format = "ogg"
+    let path = name + "."+ format
+
+    const filePath = await new Promise((resolve, reject) => {
+        ttsService.convertTextToSpeech(text,name,format,lang,function (err, result) {
+            if(err){ reject(error) }
+            resolve(path)
+        })
+    })
+
+    return FileAnswer.fromPath(filePath)
+
+})
+
+app.start();
